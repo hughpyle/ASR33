65a206598d89fa85a637644583950582f3581b4f
Hugh Pyle <hughpyle@gmail.com>
2018-09-25 12:43:39 -0400

    Updated teensy sketch

--- teensytty/teensytty.ino
+++ teensytty/teensytty.ino
@@ -1,22 +1,23 @@
 
 /**
- * Teletype Model 33 USB driver using Teensy 3
+ * Teletype Model 33 USB driver using Teensy 3.0
  *
  * ASR33 teletype connects to the hardware serial port, via 20mA current-loop.
  * USB serial connects to the host system.
  * This driver performs character translation and other functions.
+ * 
+ * 2018-09-03 update for with arduino 1.8.6
  */
 
-
-// TODO 'esc' may be codepoint 126, check & send as modern ESC
-
+// ===========================================================================
 
 // The teletype's serial interface is a 20mA current loop:
 //   110 baud
-//   One start bit
+//   "mark" when not transmitting
+//   One start bit (space)
 //   7 data bits
-//   Even parity (one bit)
-//   Two stop bits
+//   Even parity (one bit, mark)
+//   Two stop bits (mark)
 // So the total frame size is 11 bits.
 //
 // The UART does not have direct support for even parity plus two stop bits.
@@ -37,6 +38,7 @@
 //
 // But modern kernels (e.g. Darwin) have removed support for some of these stty
 // options, so it's more convenient and more flexible to do here in firmware.
+// On OSX you will still want `stty 
 //
 // The ASR33's ASCII (ANSI X3.4-1963) differs from today's ASCII in other ways.
 // Codepoint 0x5e is "up arrow", not caret (^).
@@ -59,43 +61,94 @@
 //
 // Additionally there's a pushbutton that triggers various test sequences.
 
-// set to add some debug messages to the USB serial port
+// ===========================================================================
+
+// Test Setup instructions (OSX):
+// - Assume Arduino 1.8.6 and Teensyduino are installed.
+// - In the Arduino UI:
+//      Tools -> Board -> Teensy 3.0
+//      Tools -> USB type -> Serial
+// - Verify and upload to the teensy controller.
+// - Still in the arduino UI, choose the serial port
+//      Tools -> Port -> /dev/cu.usbmodemNNN
+// - Then you should be able to run,
+//      Tools -> Serial Monitor
+
+// ===========================================================================
+
+// ASR33 terminal strip 3 & 4 are "tty send"
+// - data sent from the teletype, which is not polarity sensitive
+// - keyboard and tape reader
+// - connect these to the "Rx" (teensy input)
+//
+// ASR33 terminal strip 6 (neg) & 7 (pos) are "tty rcv"
+// - data received by teletype
+// - printer and tape punch
+// - connect these to "Tx" (teensy output)
+
+// ===========================================================================
+
+//#define CONFIG_TIMER_PRESCALE_256
+#include <AltSoftSerial.h>
+
+// pin 20 receive
+// pin 21 transmit
+
+// Uncomment this to run with "debug" messages.  It prints various "helpful" messages to the
+// USB serial port to help with debugging the code.
 // #define DEBUG_ALL
 
-// set for "local" testing (USB-only; e.g. `sudo cu -l cu.usbmodem63191`)
+// Uncomment this to run "testing mode": development without a tty attached
 // #define TESTING
 
 #ifdef TESTING
 #define HWSERIAL Serial
 #else
-// set this to the hardware serial port being used for the TTY
-#define HWSERIAL Serial1
+// Use the modified AltSoftSerial with inverted data frame
+AltSoftSerial altSerial(1,1,true);
+#define HWSERIAL altSerial
 #endif
 
 // set these to the appropriate mode selector pins (DIP switch to ground)
 #define PIN_TX_UCLC        2
 #define PIN_TX_FOLDSLASH   3
 #define PIN_TX_UTF8ARROWS  4
-#define PIN_TX_SPARE2      5
-#define PIN_RX_NLCR        7
-#define PIN_RX_EVENPARITY  8
-#define PIN_RX_DELAYS      9
-#define PIN_RX_SPARE2      10
+#define PIN_TX_8BITCLEAN   5
+#define PIN_RX_8BITCLEAN   7
+#define PIN_RX_NLCR        8
+#define PIN_RX_EVENPARITY  9
+#define PIN_RX_DELAYS      10
 
 // Pushbutton for sending test sequences (momentary switch to ground)
-#define PIN_PUSH_TEST    12
+#define PIN_PUSH_TEST      11
+#define PIN_PUSH_SPARE     12
 
 #define PIN_INTERNAL_LED 13
 
 #define CR '\r'
 #define LF '\n'
 
+// You can tweak the baud rate very slightly here, but it's better to use 110 and
+// fix whatever timing/alignment issues in hardware
+#define BAUDRATE 109
+
+elapsedMillis millisSinceActivity;
+
+
+// Pressing he "test pushbutton" runs a series of printer tests,
+// each press cycles to the next one. 
+// testId is the ID of the currently-running test.
+int testId = 0;
+
+bool isTestMode;
 bool isTxUCLC;
 bool isTxFoldSlash;
 bool isTxUtf8Arrows;
+bool isTx8BitClean;
 bool isRxNLCR;
 bool isRxEvenParity;
 bool isRxDelays;
+bool isRx8BitClean;
 
 int prevUSBByte;
 int prevTTYByte;
@@ -107,12 +160,15 @@ void setup() {
   pinMode(PIN_TX_UCLC,       INPUT_PULLUP);
   pinMode(PIN_TX_FOLDSLASH,  INPUT_PULLUP);
   pinMode(PIN_TX_UTF8ARROWS, INPUT_PULLUP);
+  pinMode(PIN_TX_8BITCLEAN,  INPUT_PULLUP);
   pinMode(PIN_RX_NLCR,       INPUT_PULLUP);
   pinMode(PIN_RX_EVENPARITY, INPUT_PULLUP);
   pinMode(PIN_RX_DELAYS,     INPUT_PULLUP);
+  pinMode(PIN_RX_8BITCLEAN,  INPUT_PULLUP);
 
   // Test pushbutton is to ground
   pinMode(PIN_PUSH_TEST,     INPUT_PULLUP);
+  pinMode(PIN_PUSH_SPARE,    INPUT_PULLUP);
 
   // Internal LED is used as a signal
   pinMode(PIN_INTERNAL_LED,  OUTPUT);
@@ -122,8 +178,13 @@ void setup() {
 
 #ifndef TESTING
   // TTY is 110 baud, 8-bit-word, two stop bits, no parity
-  HWSERIAL.begin(110, SERIAL_8N2);
+  HWSERIAL.begin(BAUDRATE); //, SERIAL_8N2);
 #endif
+
+  millisSinceActivity = 0;
+  
+  delay(100);
+  readOptions();
 }
 
 
@@ -132,88 +193,146 @@ void setup() {
 void loop() {
   int incomingByte;
 
-  if (!digitalRead(PIN_PUSH_TEST)) {
+  if (isTestMode) {
+    // print a test sequence
     digitalWrite(PIN_INTERNAL_LED, HIGH);
     selfTest();
   }
   digitalWrite(PIN_INTERNAL_LED, LOW);
 
-  isTxUCLC       = !digitalRead(PIN_TX_UCLC);
-  isTxFoldSlash  = !digitalRead(PIN_TX_FOLDSLASH);
-  isTxUtf8Arrows = !digitalRead(PIN_TX_UTF8ARROWS);
-  isRxNLCR       = !digitalRead(PIN_RX_NLCR);
-  isRxEvenParity = !digitalRead(PIN_RX_EVENPARITY);
-  isRxDelays     = !digitalRead(PIN_RX_DELAYS);
-
   if (HWSERIAL.available() > 0) {
+    millisSinceActivity = 0;
+    waitAlign();
     incomingByte = HWSERIAL.read();
     incomingByte = processTTYByte(incomingByte);
   }
 
   if (Serial.available() > 0) {
+    millisSinceActivity = 0;
+    waitAlign();
     incomingByte = Serial.read();
     incomingByte = processUSBByte(incomingByte);
+  } 
+
+  if (millisSinceActivity > 1000) {
+    waitAlign();
+    HWSERIAL.setBreak();
   }
 
 }
 
 
-// ---- Bytes from USB to TTY
-
-int processUSBByte(int b)
-{
-#ifdef DEBUG_ALL
-    Serial.print("Received from USB: ");
-    Serial.write(b);
-    Serial.print("= 0x");
-    Serial.println(b, HEX);
-#endif
-
-    if (isRxNLCR) {
-      // Linefeed conversion, incoming LF => send CR+LF to teletype
-      if ((b==LF) && (prevUSBByte!=CR)) {
-        // write a CR first
-        sendToTTY(CR);
-      }
-    }
-
-    prevUSBByte = b;
-    sendToTTY(b);
-    return b;
+void waitAlign() {
+  // align to 10th of a second to spare the clutch
+  while ((millisSinceActivity % 100) != 0) {}
 }
 
 
+void readOptions() {
+  isTestMode     = !digitalRead(PIN_PUSH_TEST);
+  isTxUCLC       = !digitalRead(PIN_TX_UCLC);
+  isTxFoldSlash  = !digitalRead(PIN_TX_FOLDSLASH);
+  isTxUtf8Arrows = !digitalRead(PIN_TX_UTF8ARROWS);
+  isTx8BitClean  = !digitalRead(PIN_TX_8BITCLEAN);
+  isRxNLCR       = !digitalRead(PIN_RX_NLCR);
+  isRxEvenParity = !digitalRead(PIN_RX_EVENPARITY);
+  isRxDelays     = !digitalRead(PIN_RX_DELAYS);
+  isRx8BitClean  = !digitalRead(PIN_RX_8BITCLEAN);
+
+#ifdef DEBUG_ALL
+  Serial.print("Tx 8-bit clean? ");  Serial.println(yesno(isTx8BitClean));
+  if (!isTx8BitClean) {
+    Serial.print("Tx UC->LC fold? ");  Serial.println(yesno(isTxUCLC));
+    Serial.print("Tx Fold escape? ");  Serial.println(yesno(isTxFoldSlash));
+    Serial.print("Tx UTF8 arrows? ");  Serial.println(yesno(isTxUtf8Arrows));
+  }
+  Serial.print("Rx LF -> CR+LF? ");  Serial.println(yesno(isRxNLCR));
+  Serial.print("Rx Even Parity? ");  Serial.println(yesno(isRxEvenParity));
+  Serial.print("Rx CRLF Delays? ");  Serial.println(yesno(isRxDelays));
+  Serial.print("Rx 8-bit clean? ");  Serial.println(yesno(isRx8BitClean));
+#endif
+}
+
+
+// ---- Bytes from TTY to USB
+
 void printTTY(const char *s) {
-  // Print a string to the teletype
-  for(int i=0; i<strlen(s); s++) {
+  for(uint i=0; i<strlen(s); s++) {
     sendToTTY(s[i]);
   }
 }
 
 
-void sendToTTY(int b) {
-  // Print a character to the teletype
-  // Includes appropriate parity and delays
-  bool pbit = 1;
-  if (isRxEvenParity) {
-    pbit = parity(b);
+int processUSBByte(int b)
+{
 #ifdef DEBUG_ALL
-    Serial.print("Parity ");
-    Serial.println(pbit, DEC);
+  Serial.print("Received from USB: ");
+  Serial.write(b);
+  Serial.print(" = 0x");
+  Serial.println(b, HEX);
 #endif
+
+  if (!isRx8BitClean) {
+    if (isRxNLCR) {
+      // TODO linefeed conversion, LF => CR+LF
+      if ((b==LF) && (prevUSBByte!=CR)) {
+        // write a CR first
+        sendToTTY(CR);
+      }
+    }
   }
-  // TODO set the parity bit
 
+  prevUSBByte = b;
+  sendToTTY(b);
+  return b;
+}
+
+
+void sendToTTY(int b) {
+
+  if (!isRx8BitClean) {
+    // When sending to the tty, normally the parity bit will be set '1'
+    // Optionally can set even-parity
+    bool pbit = 1;
+    if (isRxEvenParity) {
+      pbit = parity(b);
+#ifdef DEBUG_ALL
+//    Serial.print("Parity ");
+//    Serial.println(pbit, DEC);
+#endif
+    }
+    // TODO set the parity bit
+  }
+
+#ifdef DEBUG_ALL
+  Serial.print("Sending to TTY: ");
+  Serial.write(b);
+  Serial.print(" = 0x");
+  Serial.println(b, HEX);
+#endif
+
+  // Print the received byte at the teletype
+  // HWSERIAL.flush();
+  while (HWSERIAL.isReading()) {}
+  HWSERIAL.write((uint8_t)0);
+  digitalWrite(PIN_INTERNAL_LED, HIGH);
   HWSERIAL.write(b);
+  while (HWSERIAL.isWriting()) {}
+  digitalWrite(PIN_INTERNAL_LED, LOW);
+  delay(1);
 
+  // CR/LF delays apply even on the 8-bit-clean path
   if (isRxDelays) {
     if (b==CR) {
-      // Add delay after carriage return
-      delay(250);
+      // Add delay after carriage return (200m or more)
+      sendToTTY(0);
+      sendToTTY(0);
+      //sendToTTY(0);
     }
     if (b==LF) {
       // Add delay after line feed
-      delay(200);
+      sendToTTY(0);
+      //sendToTTY(0);
     }
   }
 
@@ -223,82 +342,89 @@ void sendToTTY(int b) {
 }
 
 
-// ---- Bytes from TTY to USB
-
 int processTTYByte(int b) {
   int c = b;
+
 #ifdef DEBUG_ALL
   Serial.print("Received from TTY: ");
   Serial.write(b);
-  Serial.print("= 0x");
-  Serial.println(b, HEX);
+  Serial.print(" = 0x");
+  Serial.print(b, HEX);
+  Serial.print(" = ");
+  Serial.println(c, BIN);
 #endif
 
-  if(isTxUCLC) {
-    // TTY produces uppercase alpha, fold them to lowercase
-    if (b>='A' && b<='Z') {
-      c = b + ('a' - 'A');
-    }
-  }
+  if (!isTx8BitClean) {
+    // Processing
 
-  if (isTxUtf8Arrows) {
-    if (b=='\x5E') {
-      // caret => up arrow
-      sendToUSB('\xE2');
-      sendToUSB('\x86');
-      c = '\x90';
-    }
-    if (b=='\x5F') {
-      // underscore => left arrow
-      sendToUSB('\xE2');
-      sendToUSB('\x86');
-      c = '\x91';
-    }
-  }
+    // Ryemove the parity bit
+    b = b & 0x7F;
 
-  // \n -> N
-  // \7 -> \7
-  // \\ -> \\ (and no escaping of the next character)
-  if (isTxFoldSlash) {
-    if (isEscapingTTY) {
-      // Previous character was backslash (and was not sent).
-      // It might 'escape' the current character.
-      if (c>='A' && c<='Z') {
-        // The current character is uppercase alpha, send as lowercase
-        c = c + ('a' - 'A');
-      } else if (c>='a' && c<='z') {
-        // The current character is lowercase alpha, send as uppercase
-        c = c - ('a' - 'A');
-      } else {
-        // Otherwise: send the slash and then the current character unchanged
-        sendToUSB(prevTTYByte);
+    if(isTxUCLC) {
+      // TTY produces uppercase alpha, fold them to lowercase
+      if (b>='A' && b<='Z') {
+        c = b + ('a' - 'A');
       }
-      isEscapingTTY = false;
-    } else if (b=='\x5C') {
-      // This is a backslash.  Don't send it yet.
-      isEscapingTTY = true;
-      c = -1;
+    }
+  
+    if (isTxUtf8Arrows) {
+      if (b=='\x5E') {
+        // caret => up arrow
+        sendToUSB('\xE2');
+        sendToUSB('\x86');
+        c = '\x90';
+      }
+      if (b=='\x5F') {
+        // underscore => left arrow
+        sendToUSB('\xE2');
+        sendToUSB('\x86');
+        c = '\x91';
+      }
+    }
+  
+    // \n -> N
+    // \7 -> \7
+    // \\ -> \\ (and no escaping of the next character)
+    if (isTxFoldSlash) {
+      if (isEscapingTTY) {
+        // Previous character was backslash (and was not sent).
+        // It might 'escape' the current character.
+        if (c>='A' && c<='Z') {
+          // The current character is uppercase alpha, send as lowercase
+          c = c + ('a' - 'A');
+        } else if (c>='a' && c<='z') {
+          // The current character is lowercase alpha, send as uppercase
+          c = c - ('a' - 'A');
+        } else {
+          // Otherwise: send the slash and then the current character unchanged
+          sendToUSB(prevTTYByte);
+        }
+        isEscapingTTY = false;
+      } else if (b=='\x5C') {
+        // This is a backslash.  Don't send it yet.
+        isEscapingTTY = true;
+        c = -1;
+      }
     }
   }
 
   prevTTYByte = b;
-  sendToUSB(c);
+
+  // Don't send nulls to the USB terminal
+  if (c != 0) {
+    sendToUSB(c);
+  }
   return c;
 }
 
-
 void sendToUSB(int c) {
-  // Print a character to the USB port
   if (c>0) {
     Serial.write(c);
   }
 }
 
 
-// ---- Misc
-
 bool parity(uint8_t v) {
-  // Compute the parity bit
   bool p = false;
   while (v) {
     p = !p;
@@ -308,7 +434,7 @@ bool parity(uint8_t v) {
 }
 
 
-char *yesno(bool f) {
+const char *yesno(bool f) {
   if (f) return "yes";
   return "no";
 }
@@ -321,31 +447,28 @@ char *yesno(bool f) {
 
 int options = 0;
 int prevOptions = -1;
-int testMode = 1;
 
 void selfTest() {
 
+  readOptions();
   options = isTxUCLC + (isTxFoldSlash<<1) + (isTxUtf8Arrows<<2)
             + (isRxNLCR<<4) + (isRxEvenParity<<5) + (isRxDelays<<6);
   if (options==prevOptions) {
-    testMode++;
+    // start the next test
+    testId++;
   } else {
-    Serial.print("Tx UC->LC fold? ");  Serial.println(yesno(isTxUCLC));
-    Serial.print("Tx Fold escape? ");  Serial.println(yesno(isTxFoldSlash));
-    Serial.print("Tx UTF8 arrows? ");  Serial.println(yesno(isTxUtf8Arrows));
-    Serial.print("Rx LF -> CR+LF? ");  Serial.println(yesno(isRxNLCR));
-    Serial.print("Rx Even Parity? ");  Serial.println(yesno(isRxEvenParity));
-    Serial.print("Rx CRLF Delays? ");  Serial.println(yesno(isRxDelays));
+    // the DIP-switches are changed, start the tests over
+    testId = 1;
   }
   prevOptions = options;
 
   // "0        1         2         3         4         5         6         7  "
   // "123456789012345678901234567890123456789012345678901234567890123456789012"
   //
-  switch(testMode) {
+  switch(testId) {
     case 1:
       // Just type all the standard printable characters.
-      printTTY("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&^()*+,-./:;<=>?@[\]^_\r\n");
+      printTTY("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&^()*+,-./:;<=>?@[]^_\r\n");
       break;
 
     case 2:
@@ -356,15 +479,15 @@ void selfTest() {
 
     case 3:
       // Space test
-      printTTY(" / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /\r");
-      for (int i=0; i<72; i+=2) {
-        for (int j=0; j<i; j++) {
-          sendToTTY(' ');
-        }
-        sendToTTY('\\');
-        sendToTTY(CR);
-      }
-      sendToTTY(LF);
+      printTTY(" / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /\r\n");
+      //for (int i=0; i<72; i+=2) {
+      //  for (int j=0; j<i; j++) {
+      //    sendToTTY(' ');
+      //  }
+      //  sendToTTY('\\');
+      //  sendToTTY(CR);
+      //}
+      //sendToTTY(LF);
       break;
 
     case 4:
@@ -401,7 +524,7 @@ void selfTest() {
 
     default:
       printTTY("Tests complete.\r\n");
-      testMode = 0;
+      testId = 0;
       break;
   }
   delay(500);
