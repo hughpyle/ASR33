3719684d242d3efdbeccd74eb9330661bcc57f7c
Hugh Pyle <hpyle@cabezal.com>
2019-02-18 15:21:04 +0000

    add comment.  also: spaces not tabs!

--- bin/hc.c
+++ bin/hc.c
@@ -3,18 +3,20 @@
 /* compile with "gcc $(pkg-config --cflags --libs ncurses) hc.c -o hc" */
 /* pls excuse the <%...%> alternative tokens, teletype 33 has no curly braces */
 int main() <%
-	int errret;
-	setupterm((char *)0, /* fd */1, &errret);
-	int hc = tigetflag("hc");
-	switch(hc) <%
-		case -1:
-			puts("no information available");
-			break;
-		case 0:
-			puts("not a hardcopy terminal");
-			break;
-		default:
-			puts("this is a hardcopy terminal");
-			break;
-	%>
+    int errret, hc;
+    /* initialize curses, but ignore the return value */
+    setupterm((char *)0, /* fd */1, &errret);
+    /* now test do we have hardcopy */
+    hc = tigetflag("hc");
+    switch(hc) <%
+        case -1:
+            puts("no information available");
+            break;
+        case 0:
+            puts("not a hardcopy terminal");
+            break;
+        default:
+            puts("this is a hardcopy terminal");
+            break;
+    %>
 %>
